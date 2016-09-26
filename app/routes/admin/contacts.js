9a9d40630121de5cc6cbb67c882ecaeca13adac6
scroobius-pip <sim04ful@gmail.com>
2016-09-26 18:42:19 +0100

    current

--- app/routes/admin/contacts.js
+++ app/routes/admin/contacts.js
@@ -4,5 +4,14 @@ export default Ember.Route.extend({
     model() {
         return this.store.findAll('contact');
     },
- 
+    actions: {
+        deleteContacts(contact) {
+            let confirmation = confirm("Delete?");
+            if (confirmation) {
+                contact.destroyRecord()
+
+            }
+        }
+    }
+
 });
--- app/routes/admin/invitations.js
+++ app/routes/admin/invitations.js
@@ -1,7 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model(){
-    return this.store.findAll('invitation');
+    model() {
+        return this.store.findAll('invitation');
+    },
+    actions: {
+        deleteInvitation(invitation) {
+            let confirmation = confirm("Delete invitation?");
+            if (confirmation) {
+                invitation.destroyRecord();
+            }
+        }
     }
 });
